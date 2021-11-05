7984e3730df91e62976dffb2514d5f597bf3e91e
theturboturnip <popgoestoast@gmail.com>
2021-11-05 00:42:32 +0000

    Added most of ex1 c

--- assignment1/ex_1a.m
+++ assignment1/ex_1a.m
@@ -112,4 +112,75 @@ title("1.b.iii) Backwards Difference");
 
 
 %% 1.c
+% Generate a one second long Gaussian noise sequence r with a sampling rate
+% of 300Hz
+f_s = 300;
+t = 1; % 1 second long
+N = f_s * t;
+ts = linspace(0, t, N);
+r = randn(N,1);
+% Taper $r$ by setting its first and last 15 samples to zero.
+r(1:15) = 0;
+r(end-15:end) = 0;
 
+%%
+% Make a Finite-Impulse Response low-pass filter with cut-off frequency
+f_c = 45; % cutoff frequency
+a = [1]; % FIR has no y-terms
+b = fir1(50, f_c/(f_s/2));
+% Use the filtfilt function in order to apply that filter to the
+% generated noise signal, resulting in the filtered noise signal x
+x = filtfilt(b, a, r);
+
+%%
+% Sample x at 100Hz by setting all but every third sample value to zero
+y = x;
+% element 1, 4, 7... = 0
+y(1:3:end) = 0;
+% element 2, 5, 8... = 0
+y(2:3:end) = 0;
+% element 3, 6, 9... = unchanged
+
+%%
+% Implement sinc interpolation to reconstruct the zeroed samples of y
+%
+% $$x(t) = \sum_{n=-\infty}^{\infty} x_n * sinc(t/t_s - n)$$
+%
+% MATLAB version:
+% $$z = \sum_{i_y=1}^{N} y(i_y) * sinc((ts/t - i_y/N) * f_s/3)$$
+%
+% Translate the sinc by i_y/N to align it with the sample,
+% and scale it by f_s over 3 to align the zero crossings with the other
+% sample points
+z = zeros(N,1);
+figure;
+stem(y)
+hold on;
+for i_y = 1:N
+    data = y(i_y) * sinc((ts/t - i_y/N) * f_s/3);
+    plot(data);
+    z = z + data';
+end
+hold off;
+
+%%
+% Generate another low-pass filter with fir1, cut-off frequency 50Hz
+f_c = 50; % cutoff frequency
+a = [1]; % FIR has no y-terms
+b = fir1(50, f_c/(f_s/2));
+% Apply it to y, resulting in interpolated sequence u.
+% multiply by 3 to compensate for energy lost during sampling.
+u = 3 * filtfilt(b, a, y);
+
+
+%%
+% Plot x, y, z, and u on top of each other in one figure 
+plot(x);
+hold on;
+stem(y);
+plot(z);
+plot(u);
+hold off;
+
+%% 
+% TODO Compare x, y, z, u
