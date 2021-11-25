24638e2fcffa79f32740d09c62a06064b101c1ac
theturboturnip <popgoestoast@gmail.com>
2021-11-25 21:28:49 +0000

    Did 3bi

--- assignment3/ex_3b.m
+++ assignment3/ex_3b.m
@@ -10,9 +10,33 @@ z = c(1,:) + 1j*c(2,:);
 Fs = 240000;
 
 %%
-% TODO FM demodulate
-
+% FM demodulate (see function)
+s = fm_demodulate(Fs, z);
+plot(s)
 
 %%
 % Low-pass filter
-butter()
+F_low = 16000;
+[b,a] = butter(10, (F_low/(Fs/2)), 'low');
+h = fvtool(b, a);
+s_filtered = filter(b, a, s);
+
+%%
+% Reduce from 240kHz to 48kHz
+s_reduced = s_filtered(1:5:end);
+
+%%
+% Normalize, write out to audio
+s_normal = normalize(s_reduced, 'range', [-1 1]);
+audiowrite('ex_3b_i.wav', s_normal, 48000);
+
+%% Functions
+
+function s = fm_demodulate(Fs, z)
+    dt = 1/Fs;
+    s = angle(z(2:end)./z(1:(end-1)))/dt;
+end
+
+function z_prime=shift_freq(z, f, fs)
+
+end
